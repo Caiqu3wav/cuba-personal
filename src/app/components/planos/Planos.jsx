215f559ead6ef674e89eb9d81addc8444ca44920
Caique <asapcaique7@gmail.com>
2025-05-22 13:23:30 -0300

    complete new layout price contents

--- src/app/components/planos/Planos.jsx
+++ src/app/components/planos/Planos.jsx
@@ -10,102 +10,85 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import Link from "next/link"
 import "../../../styles/planos.css"
-import planos from '@/data/index'
+import { IoMdCheckmarkCircleOutline } from "react-icons/io"
+import { planos } from '@/data/index'
+import {Star} from 'lucide-react'
 
 export default function Planos() {
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    let mm = gsap.matchMedia(), breakPoint = 800;
-
-    mm.add(
-      {
-        isDesktop: `(min-width: ${breakPoint}px)`,
-        isMobile: `(max-width: ${breakPoint - 100}px)`,
-        isPad: `(min-height: ${breakPoint - 1}px)`
-      },
-      (context) => {
-        let { isDesktop, isMobile, isPad } = context.conditions;
-
-
-        gsap.to(".lateral-cards", {
-          x: 0,
-          y: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: "#planos",
-            endTrigger: ".footer",
-            start: isMobile ? "top 740" : isPad ? 'top 1150' : "top 510",
-            end: isMobile ? "top 870" : isPad ? 'top 1250' : "top 590",
-            scrub: true,
-          }
-        })
-
-        gsap.to(".center-card", {
-          x: 0,
-          y: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: "#planos",
-            endTrigger: ".footer",
-            start: isMobile ? "top 700" : isPad ? 'top 1120' : "top 430",
-            end: isMobile ? "top 870" : isPad ? 'top 1250' : "top 500",
-            scrub: true,
-          }
-        })
-      })
-  });
-
+  
   return (
-    <div id="planos" className="pb-4 h-[570px] majorfour1:h-fit majorfour1:pb-7 midtwo2:pb-12 flex flex-col items-center justify-center w-full">
+    <div id="planos" className="pb-4 h-fit  flex flex-col items-center justify-center w-full">
       <h1 className="text-[40px] font-light text-white">PLANOS</h1>
       <hr className=" border-solid border-blue-600 rounded-lg border-2 border-x-[300px]" />
 
-      <div className="grid grid-cols-3 gap-6">
-        <div className="lateral-cards planos-cards translate-x-[-300px] translate-y-[-300px] bg-slate-300 opacity-0 w-[300px] transition-all duration-400 
-                hover:scale-125 cursor-pointer flex flex-col items-center  mt-4 text-black rounded-lg py-4 px-3">
-          <Image src={Consult} className="transition-all rounded-md w-[90%] h-[255px] midtwo:w-[170px] midtwo:h-[170px] duration-700  hover:opacity-75" alt="" />
-          <div className="flex flex-col items-center justify-between text-[22px] font-semibold text-center h-[200px]
-                    majorthree2:h-[120px] midtwo2:h-[85px]">
-            <h1 className="flex items-center gap-2"><span className="majorthree:text-sm">Consultoria Online</span> <AiOutlineGlobal className="text-blue-600" /> </h1>
-            <button className="px-14 rounded-xl bg-red-300/60 text-red-500 font-bold py-1">R$ 199/mês</button>
-            <p className="text-sm text-gray-500 px-4">Treinos personalizados para você alcançar seus objetivos no seu ritmo.</p>
+      <div className="w-full flex mt-4 justify-around gap-6 flex-wrap">
+        <div className="planos-cards bg-slate-300 w-[300px] h-fit transition-all duration-400 
+                hover:scale-125 cursor-pointer flex flex-col gap-1 items-center justify-center text-black rounded-lg py-4 px-3">
+          <Image src={Consult} className="transition-all rounded-md w-[90%] h-[255px] duration-700  hover:opacity-75" alt="" />
+          <div className="flex flex-col items-center justify-between text-[22px] gap-2 font-semibold text-center h-fit">
+            <h1 className="flex items-center gap-2"><span className="">Consultoria Online</span> <AiOutlineGlobal className="text-blue-600" /> </h1>
+            <button className="px-14 rounded-xl bg-red-300/60 text-red-500 text-[15px] font-bold py-1">R$ 199/mês</button>
+            <ul className="space-y-3 text-[15px]">
+              {planos.consultoria.map((feature, index) => (
+                <li key={index} className="flex items-start gap-1">
+                  <IoMdCheckmarkCircleOutline className="h-5 w-5 text-red-400 flex-shrink-0"/>
+                  <span className="text-gray-700">{feature}</span>
+                  </li>
+              ))}
+            </ul>
             <Link className="w-[70%]" href="https://wa.me/5512981359114?text=Olá%2C+gostaria+de+saber+mais+sobre+a+consultoria+online!">
-            <button className=" w-[100%] text-[17px] midtwo2:text-[14px] py-2 rounded-lg bg-black text-white transition-all duration-700
-            hover:bg-white hover:text-black text-nowrap">SAIBA MAIS</button>
+            <button className=" w-[100%] text-[17px] py-2 rounded-lg bg-black text-white transition-all duration-700
+            hover:bg-white hover:text-black text-nowrap">Consultar Plano</button>
             </Link>
           </div>
         </div>
-        <div className="center-card planos-cards bg-slate-300 text-black flex flex-col items-center self-end translate-y-[200px] opacity-0 w-[300px] transition-all duration-400 
-                hover:scale-125 cursor-pointer mt-4 majorfour1:mt-0 rounded-lg py-4 px-3">
-          <Image src={Treino} className="transition-all rounded-md w-[90%] h-[255px] midtwo:w-[170px] midtwo:h-[170px]  duration-700 hover:opacity-75" alt="" />
-          <div className="flex flex-col items-center text-[22px] font-semibold text-black
-                    majorthree2:h-[120px] text-center h-[200px] midtwo2:h-[85px]">
-            <h1 className="midtwo2:text-[14px] flex items-center gap-2"><span className="majorthree:text-sm">Personal Trainer</span> 
+        <div className="planos-cards bg-slate-300 text-black flex flex-col items-center w-[300px] h-fit transition-all duration-400 
+                hover:scale-125 cursor-pointer border-2 border-red-600 rounded-lg py-4 px-3">
+          <Image src={Treino} className="transition-all rounded-md w-[90%] h-[255px] duration-700 hover:opacity-75" alt="" />
+          <div className="flex flex-col items-center gap-2 text-[22px] font-semibold text-black text-center h-fit">
+            <h1 className="flex items-center gap-2"><span className="">Personal Trainer</span> 
             <Image src={GymLogo} className="w-[30px]" alt="gym logo" />
             </h1>
-                        <button className="px-14 rounded-xl bg-red-300/60 text-red-500 font-bold py-1">R$ 299/mês</button>
+                        <button className="px-14 rounded-xl bg-red-300/60 text-[15px] text-red-500 font-bold py-1">R$ 299/mês</button>
 
-            <p className="text-sm text-gray-500 px-4">Acompanhamento profissional para maximizar seus treinos e resultados.</p>
+            <ul className="space-y-3 text-[15px]">
+              {planos.personal.map((feature, index) => (
+                <li key={index} className="flex items-center gap-1">
+                  <IoMdCheckmarkCircleOutline className="h-5 w-5 text-red-400 flex-shrink-0"/>
+                  <span className="text-gray-700">{feature}</span>
+                  </li>
+              ))}
+            </ul>
             <Link href="https://wa.me/5512981359114?text=Olá%2C+gostaria+de+saber+mais+sobre+seus+serviços!"
               className="w-[70%]">
-              <button className="w-[70%] text-[17px] midtwo2:text-[14px] py-2 rounded-lg bg-black text-white transition-all duration-700
-                         hover:bg-white hover:text-black">SAIBA MAIS</button></Link>
+              <button className="w-full text-[17px] py-2 rounded-lg bg-black text-white transition-all duration-700
+                         hover:bg-white hover:text-black">Consultar Plano</button></Link>
           </div>
         </div>
-        <div className="lateral-cards planos-cards w-[300px] bg-slate-300 text-black majorfour1:mt-7 translate-x-[300px] translate-y-[-300px] opacity-0
-                      transition-all duration-400 hover:scale-125 cursor-pointer mt-4  rounded-lg py-4 px-3">
-          <Image src={BfImg} className="transition-all rounded-md w-[90%] h-[255px] midtwo:w-[170px] midtwo:h-[170px] duration-700 hover:opacity-75 " alt="" />
-          <div className="flex flex-col items-center text-[22px] font-semibold text-black 
-                    majorthree2:h-[120px] text-center h-[100px] midtwo2:h-[85px]">
-            <h1 className="midtwo2:text-[14px] majorthree:text-sm">Plano BlackFit ⭐</h1>
-                        <button className="px-14 rounded-xl bg-red-300/60 text-red-500 font-bold py-1">R$ 449/mês</button>
-            <p className="text-sm text-gray-500 majorthree:text-xs px-4">
-              O melhor dos dois mundos: acompanhamento completo com Personal Trainer <b>Cuba</b> e Nutricionista <b>Allan</b>.
-            </p>
-            <Link href="https://wa.me/5512981359114?text=Olá%2C+gostaria+de+saber+mais+sobre+o+plano+completo+personal+e+nutricionista!"
-              className="w-[70%] text-[17px] midtwo2:text-[14px] py-2 rounded-lg bg-black text-white transition-all duration-700
-                         hover:bg-white hover:text-black">SAIBA MAIS</Link>                    </div>
+        <div className="relative planos-cards w-[300px] bg-gradient-to-r h-fit from-black to-slate-800 text-black transition-all 
+        duration-400 hover:scale-125 cursor-pointer flex flex-col items-center  rounded-lg py-4 px-3">
+          <Image src={BfImg} className="transition-all rounded-md w-[90%] h-[255px] duration-700 hover:opacity-75 " alt="" />
+          <div className="flex flex-col items-center gap-2 text-[22px] font-semibold text-white text-center h-fit">
+                  <div className="absolute -top-4 left-0 right-0 flex justify-center">
+                <div className="bg-yellow-400 text-black px-4 py-1 rounded-full flex items-center text-sm font-bold">
+                  <Star className="h-4 w-4 mr-1" fill="currentColor" /> PREMIUM
+                </div>
+              </div>
+            <h1 className="">Plano BlackFit ⭐</h1>
+            <p className="text-sm text-gray-600">O melhor dos dois mundos</p>
+                        <button className="px-14 rounded-xl text-[15px] bg-gray-400/60 text-white font-bold py-1">R$ 449/mês</button>
+            <ul className="space-y-3 text-lg">
+              {planos.blackfit.map((feature, index) => (
+                <li key={index} className="flex items-center text-center">
+                  <IoMdCheckmarkCircleOutline className="text-yellow-400 flex-shrink-0"/>
+                  <span className="text-gray-400 ml-1 text-[15px]">{feature}</span>
+                  </li>
+              ))}
+            </ul>
+            <Link href="https://wa.me/5512981359114?text=Olá%2C+gostaria+de+saber+mais+sobre+o+plano+completo+BlackFit+personal+e+nutricionista!"
+              className="w-[70%] text-[17px] py-2 rounded-lg bg-yellow-400 text-black transition-all duration-700
+                         hover:bg-white hover:text-black">Consultar Plano</Link>
+                         </div>
         </div>
       </div>
     </div>
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,40 +1,23 @@
-const blogs = [
-    {id: 1, nome: "Linguagens de programação atualidade(2024)", 
-    descr: "O blog aborda o dinâmico cenário das linguagens de programação em 2024, oferecendo insights valiosos sobre as escolhas mais relevantes para desenvolvedores.",
-    imagem: "/assets/img/programming-languages.jpg", dataLnc: '2024/01/29', link: "/actuallanguages"
-},{id: 2, nome: "Inteligência Artificial na T.I: Transformando o Desenvolvimento de Software", 
-descr: "Descubra como a inteligência artificial (IA) está revolucionando a programação, atuando como um 'segundo par de olhos' para os desenvolvedores.",
-imagem: "/assets/img/artificiaal-inteligence-computing.jpg", dataLnc: '2024/02/01', link: "/ia"
-},{id: 3, nome: "Desvendando o Mundo da Organização de Dados", 
-descr: "Explore o fascinante universo dos bancos de dados, fundamentais na era digital, onde informações cruciais são organizadas, recuperadas e gerenciadas.",
-imagem: "/assets/img/databasesrepresentations.jpg", dataLnc: '2024/02/02', link: "/bancos_de_dados"
-},
-{id: 4, nome: "O guia para um bom código", 
-descr: "Explore as fundações do desenvolvimento de software com nosso blog dedicado às boas práticas de código, baseado nas perspicazes observações de Alex Xu.",
-imagem: "/assets/img/code-analize.webp", dataLnc: '2024/02/07', link: "/goodcode"
-},
-];
-
-const planFeatures = {
+export const planos = {
     consultoria: [
       "Avaliação física online",
       "Planilha de treinos personalizada",
       "Suporte via WhatsApp",
       "Ajustes mensais",
-      "App exclusivo para acompanhamento"
+      "App de treino"
     ],
     personal: [
       "Avaliação física completa",
       "Treinos presenciais",
       "Acompanhamento exclusivo",
       "Relatórios de progresso semanais",
-      "Acesso ao app exclusivo"
+      "App de treino"
     ],
     blackfit: [
-      "Tudo do plano Personal Trainer",
-      "Acompanhamento nutricional completo",
+      "Tudo dos planos",
+      "Acompanhamento nutricional",
       "Plano alimentar personalizado",
-      "Ajustes semanais de treino e dieta",
+      "Ajustes semanais de treino/dieta",
       "Suporte prioritário 24/7"
     ]
   };
